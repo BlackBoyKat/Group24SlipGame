c5ebf1684414335fc3ed472612c0a36d45a369d6
N-bot1 <neomoshoete@gmail.com>
2026-04-02 13:39:14 +0200

    test

--- Group24Slip/Assets/Scripts/SlipMovement.cs
+++ Group24Slip/Assets/Scripts/SlipMovement.cs
@@ -5,7 +5,7 @@ public class SlipMovement : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        int apple;
     }
 
     // Update is called once per frame
