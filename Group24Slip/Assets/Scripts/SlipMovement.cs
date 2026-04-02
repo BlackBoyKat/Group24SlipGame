f6dab60b8b24b654da33e66b01a8691b584f6836
BlackBoyKat <katlegothekactus@gmail.com>
2026-04-02 14:23:27 +0200

    Variable added
    Just added the public float for the player speed as a start, i'll do more later

--- Group24Slip/Assets/Scripts/SlipMovement.cs
+++ Group24Slip/Assets/Scripts/SlipMovement.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class SlipMovement : MonoBehaviour
 {
+    public float playerSpeed = 5f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
