8eecdaa83864086e856e50673e2ac6a8433fac4a
rooibos-png <tyara.manyengavana3@gmail.com>
2026-04-16 23:38:39 +0200

    tutorial
    playtest purposes

--- Group24Slip/Assets/Scripts/Dialogue_Manager.cs
+++ Group24Slip/Assets/Scripts/Dialogue_Manager.cs
@@ -6,13 +6,12 @@ using TMPro; // Used for TextMeshPro to code the dialogue
 public class Dialogue_Manager : MonoBehaviour
 {
     public TextMeshProUGUI textComponent; // The text box
-    public string[] lines; // personal note: alt ver (text) - hardcode the script instead of using inspector to input the dialogue (uses more coroutines)
+    public string[] lines;
     public float textSpeed; // Creates the typewriter effect
-    private Coroutine typingRoutine; // This is used to stop the typewriter effect when the dialogue is finished
+
     private int index; // Paces/tracks the dialogue
     void Start()
     {
-        index = 0;
         textComponent.text = string.Empty; // This makes the text box empty at the start of the scene
 
     }
@@ -24,16 +23,8 @@ public class Dialogue_Manager : MonoBehaviour
     }
     void StartDialogue()
     {
-       if(typingRoutine == null) // Code is subject to change girl tf is going on with this
-                                 // 
-        {
-            StopCoroutine(typingRoutine);
-        }
-        Debug.Log(index);
-        Debug.Log(lines.Length);
-        index ++;
-        
-        typingRoutine =StartCoroutine(TypeLine());
+        index = 0;
+        StartCoroutine(TypeLine());
     }
 
     IEnumerator TypeLine() //this will individually type out each line/character of the dialogue
@@ -42,8 +33,6 @@ public class Dialogue_Manager : MonoBehaviour
         {
             textComponent.text += c;
             yield return new WaitForSeconds(textSpeed);
-            break;
         }
-        typingRoutine = null;
     }
 }
