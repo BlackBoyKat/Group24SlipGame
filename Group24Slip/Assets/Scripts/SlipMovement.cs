358319092cb7f0d9ee05ac1b636d7ce013597c5f
BlackBoyKat <katlegothekactus@gmail.com>
2026-05-20 13:24:13 +0200

    Better Movement and sfx, not yet done

--- Group24Slip/Assets/Scripts/SlipMovement.cs
+++ Group24Slip/Assets/Scripts/SlipMovement.cs
@@ -3,23 +3,29 @@ using UnityEngine;
 
 public class SlipMovement : MonoBehaviour
 {
+    [Header("Components")]
     private SpriteRenderer SpriteRenderer;
     private Rigidbody2D body;
 
+    [Header("Movement")]
     public float jumpPower = 4f;
-
     [SerializeField] private float slipSpeed = 5f;
     private Vector2 axisMovement;
 
-    bool isGrounded = false;
-
+    [Header("Animations")]
     private Animator animator;
     bool isJumping = false;
     bool isPushing = false;
     bool isGrinding = false;
+    bool isGrounded = false;
 
+    [Header("Better Jumping")]
     public float fallMultiplier = 2.5f;
     public float lowJumpMultiplier = 2f;
+
+    [Header("Coyote Time")]
+    public float coyoteTime = 0.2f;
+    private float coyoteTimeCounter;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -39,7 +45,17 @@ public class SlipMovement : MonoBehaviour
         isJumping = false;
         isPushing = false;
         isGrinding = false;
-        
+
+        //Coyote Time
+        if (isGrounded)
+        {
+            coyoteTimeCounter = coyoteTime; // Reset the coyote time counter when grounded
+        }
+        else
+        {
+            coyoteTimeCounter -= Time.deltaTime; // Decrease the coyote time counter when not grounded
+        }
+
 
         //This is to get the raw input from the horizontal axis, which is the left and right movement, this is done by using the Input.GetAxisRaw method, which returns a value between -1 and 1, where -1 is left, 0 is no movement and 1 is right. The raw input is used to prevent any smoothing that might be applied to the input, which can make the movement feel more responsive.
 
@@ -51,25 +67,23 @@ public class SlipMovement : MonoBehaviour
         axisMovement.x = Input.GetAxisRaw("Horizontal");
 
 
-        //Allow jump only when grounded
-        if (Input.GetButtonDown("Jump") && isGrounded) //this is a check to see if the player is pressing the jump button and if the player is not already jumping, this is to prevent the player from being able to jump multiple times in the air.
+        //Allow jump only when grounded or within coyote time
+        if (Input.GetButtonDown("Jump") && coyoteTimeCounter > 0f) //this is a check to see if the player is pressing the jump button and if the player is not already jumping, this is to prevent the player from being able to jump multiple times in the air.
         {
             body.linearVelocity = new Vector2(body.linearVelocity.x, jumpPower); // Set the vertical velocity to the jump power, this will make the player jump.
-
             isGrounded = false; // Set isGrounded to false when the player jumps 
-
             Move.y = 1f;
-            isJumping = true; // Set isJumping to true when the player jumps
-
+            isJumping = true; 
+            coyoteTimeCounter = 0f; // Reset coyote time counter when the player jumps
         }
 
         if(body.linearVelocity.y < 0) //Falling
         {
-            body.linearVelocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime; 
+            body.linearVelocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - .2f) * Time.deltaTime; 
         }
         else if(body.linearVelocity.y > 0 && !Input.GetButton("Jump")) //Rising but jump button is released
         {
-            body.linearVelocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
+            body.linearVelocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - .2f) * Time.deltaTime;
         }
 
         //Left Right movement
@@ -79,10 +93,7 @@ public class SlipMovement : MonoBehaviour
             isPushing = true;
 
             //flips without resizing
-
             SpriteRenderer.flipX = true;
-            
-
         }
 
         if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
@@ -91,8 +102,6 @@ public class SlipMovement : MonoBehaviour
             isPushing = true;
 
             SpriteRenderer.flipX = false;
-            
-
         }
 
         transform.position += Move.normalized * slipSpeed * Time.deltaTime;
